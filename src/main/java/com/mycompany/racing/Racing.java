f1a69785dd4785e8f33d9baeeb628066761b4cfa
Sebastian Ortiz <159595110+sortiz0640@users.noreply.github.com>
2025-02-15 23:50:05 -0600

    Racing.java
    Changed object names

--- src/main/java/com/mycompany/racing/Racing.java
+++ src/main/java/com/mycompany/racing/Racing.java
@@ -6,12 +6,12 @@ public class Racing extends Thread {
     private static int goal = 100;  
     public static void main(String[] args) {
                
-        Racer sebas = new Racer("Python", goal);
-        Racer julian = new Racer("Java", goal);
-        Racer tulio = new Racer("C++", goal);
+        Racer racer1 = new Racer("Python", goal);
+        Racer racer2 = new Racer("Java", goal);
+        Racer racer3 = new Racer("C++", goal);
         
-        sebas.start();
-        julian.start();
-        tulio.start();       
+        racer1.start();
+        racer2.start();
+        racer3.start();       
     }
 }
